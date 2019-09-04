855108306337e796b8ab131a27814bce39460766
Evgeniy <evgeniy.freshlife@gmail.com>
2019-09-04 23:27:25 +0300

    Initial Commit

---
+++ SecurityTeam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecurityTeam
+//
+//  Created by Evgeniy Suprun on 9/4/19.
+//  Copyright © 2019 Evgeniy Suprun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
